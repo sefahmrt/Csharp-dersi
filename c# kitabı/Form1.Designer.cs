3b83e851938fa1d3093ced41775e682a9f43cbc6
sefah <sefah@FREUDE>
2026-02-26 14:06:13 +0300

    Proje dosyası ekle.

---
+++ c# kitabı/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace c__kitabı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            textBox3 = new TextBox();
+            button3 = new Button();
+            listBox1 = new ListBox();
+            textBox4 = new TextBox();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Minecraft", 25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(144, 103);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(141, 47);
+            button1.TabIndex = 0;
+            button1.Text = "Press";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            label1.Location = new Point(52, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            label2.Location = new Point(9, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 37);
+            label2.TabIndex = 2;
+            label2.Text = "Soyad";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 15F);
+            textBox1.Location = new Point(123, 13);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(176, 34);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 15F);
+            textBox2.Location = new Point(123, 61);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(176, 34);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 30F);
+            label3.Location = new Point(347, 13);
+            label3.Name = "label3";
+            label3.Size = new Size(145, 54);
+            label3.TabIndex = 4;
+            label3.Text = "Bilgiler";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label4.Location = new Point(347, 67);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 32);
+            label4.TabIndex = 1;
+            label4.Text = "Adı:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label5.Location = new Point(304, 98);
+            label5.Name = "label5";
+            label5.Size = new Size(97, 32);
+            label5.TabIndex = 2;
+            label5.Text = "Soyadı:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label6.Location = new Point(412, 67);
+            label6.Name = "label6";
+            label6.Size = new Size(83, 32);
+            label6.TabIndex = 1;
+            label6.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label7.Location = new Point(412, 99);
+            label7.Name = "label7";
+            label7.Size = new Size(83, 32);
+            label7.TabIndex = 2;
+            label7.Text = "label7";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "1. Sınıf", "2. Sınıf", "3. Sınıf", "4. Sınıf", "5. Sınıf" });
+            comboBox1.Location = new Point(103, 404);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(196, 26);
+            comboBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(154, 436);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Hazırlık Ekle";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(103, 375);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(196, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(313, 379);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 8;
+            button3.Text = "Ekle";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Items.AddRange(new object[] { "Bilgisayar Mühendisliği", "Yazılım Mühendisliği", "Veri Analistliği", "Matematik Mühendisliği", "Grafik Tasarım" });
+            listBox1.Location = new Point(583, 282);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(162, 148);
+            listBox1.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(583, 436);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(162, 23);
+            textBox4.TabIndex = 10;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(751, 437);
+            button4.Name = "button4";
+            button4.Size = new Size(92, 23);
+            button4.TabIndex = 11;
+            button4.Text = "Listeye Ekle";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(855, 553);
+            Controls.Add(button4);
+            Controls.Add(textBox4);
+            Controls.Add(listBox1);
+            Controls.Add(button3);
+            Controls.Add(textBox3);
+            Controls.Add(button2);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Font = new Font("Sitka Subheading", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private ComboBox comboBox1;
+        private Button button2;
+        private TextBox textBox3;
+        private Button button3;
+        private ListBox listBox1;
+        private TextBox textBox4;
+        private Button button4;
+    }
+}
---
+++ c# kitabı/Form1.cs
@@ -0,0 +1,40 @@
+namespace c__kitabı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label6.Text = textBox1.Text;
+            label7.Text = textBox2.Text;
+            MessageBox.Show("İşlem Tamam");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Hazırlık");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(textBox3.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!listBox1.Items.Contains(textBox4.Text))
+                listBox1.Items.Add(textBox4.Text); 
+            else
+                listBox1.Items.Remove(textBox4.Text);
+        }
+    }
+}
